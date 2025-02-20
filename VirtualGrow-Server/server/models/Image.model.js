2447c1e78795335a2d90a154734b0736233f77f9
priya.sidhaiyan@yenlo.com <priya.sidhaiyan@yenlo.com>
2025-02-20 03:22:02 +0100

    Added Image model

---
+++ VirtualGrow-Server/server/models/Image.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const ImageSchema = new mongoose.Schema({
+  gardenName: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Image', ImageSchema);
