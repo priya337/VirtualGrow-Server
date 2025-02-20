2d623526fd181c83600e678131e34018399fb739
priya.sidhaiyan@yenlo.com <priya.sidhaiyan@yenlo.com>
2025-02-20 21:03:32 +0100

    Modified Garden.model

--- VirtualGrow-Server/server/models/Garden.model.js
+++ VirtualGrow-Server/server/models/Garden.model.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
 const gardenSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true }, // ✅ Add garden name
+  name: { type: String, required: true, unique: true },
   gardenSize: {
     length: Number,
-    breadth: Number
+    breadth: Number,
   },
   preferredPlants: [String],
-  gardenPlanOverview: Object
+  gardenPlanOverview: Object,
+  imageUrl: { type: String }, // <-- Add this line
 });
 
 const Garden = mongoose.model("Garden", gardenSchema);
